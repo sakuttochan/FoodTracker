af1f1fb4a607d98dd9b33c79f534529b3cbf7507
KazukiSakurada <sk.bumpbaske@gmail.com>
2017-09-07 17:45:37 +0900

    first commit

---
+++ FoodTracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by Kazuki Sakurada on 2017/09/07.
+//  Copyright © 2017年 Kazuki Sakurada. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+}
+
